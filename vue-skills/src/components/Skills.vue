712a54502e37504a4f79db2b608152e9d067d3a0
Jeremy VALLES <jeremy.valles@niji.fr>
2020-02-10 17:39:13 +0100

    tested a ternary (v-if v-else) if else Working

--- vue-skills/src/components/Skills.vue
+++ vue-skills/src/components/Skills.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="hello">
- {{ name }}
-{{ btnState ? 'Yes' : 'No'}}
- <button v-on:click="btnState=!btnState">Change Name</button>
+    <div class="holder">
+      <ul>
+        <li v-for="(data, index) in skills" :key='index'>{{ index }}. {{ data.skill }}</li>
+      </ul>
+
+<p v-if="skills.length > 1">You have more than 1 skills</p>
+<p v-else>You have less than or equal to 1 skill</p>
+
+       </div>
   </div>
 </template>
 
@@ -11,8 +17,10 @@ export default {
   name: 'Skills',
   data() {
     return {
-      name: 'Coursetro',
-      btnState: true
+      skills: [
+        { "skill": "Vue.js" },
+        { "skill": "Frontend Developer" }
+      ]
     }
   }
 }

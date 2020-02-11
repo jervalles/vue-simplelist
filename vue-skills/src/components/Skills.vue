65437abeda41fde6e996048d9079db6a5f4d2b50
Jeremy VALLES <jeremy.valles@niji.fr>
2020-02-11 12:13:31 +0100

    added a Checkbox to add if the Skill has an Advanced level WORKING

--- vue-skills/src/components/Skills.vue
+++ vue-skills/src/components/Skills.vue
@@ -3,10 +3,15 @@
     <div class="holder">
       <form @submit.prevent="addSkill">
         <input type="text" placeholder="Enter a skill you have.." v-model="skill">
+        <div class="advanced">Advanced
+          <input type="checkbox" id="checkbox" v-model="checked">
+        </div>
       </form>
         <p class="preview" v-if="skill.length > 0"> {{ skill }}</p>
       <ul>
-        <li v-for="(data, index) in skills" :key='index'>{{ index }}. {{ data.skill }}</li>
+        <li v-for="(data, index) in skills" :key='index'>{{ index }}. {{ data.skill }} 
+          <span v-if="data.checked">Advanced</span>
+        </li>
       </ul>
 <!-- 
 <p v-if="skills.length > 1">You have more than 1 skills</p>
@@ -25,10 +30,9 @@ export default {
   name: 'Skills',
   data() {
     return {
+      checked: false,
       skill: '',
       skills: [
-        { "skill": "Vue.js" },
-        { "skill": "Frontend Developer" }
       ],
       /*
       alertObject: {
@@ -39,8 +43,10 @@ export default {
   },
   methods: {
     addSkill() {
-      this.skills.push({skill: this.skill}),
-      this.skill = ''
+      this.skills.push({skill: this.skill, checked: this.checked}),
+      this.skill = '',
+      console.log('This checkbox value is: '+this.checked),
+      this.checked = false;
     }
   }
 }
